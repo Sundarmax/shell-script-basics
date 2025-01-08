16407e7c3cdf721d32723346a2ffe728efb35eeb
sundar <sundar.info22@gmail.com>
2025-01-08 12:51:48 +0530

    shell script variable

--- variable.sh
+++ variable.sh
@@ -8,3 +8,23 @@ num2=11
 echo $num2
 unset num2 # The unset keyword assists in removing the value from the specified variable.
 echo "Num2 value is":$num2
+
+setAge() { 
+    echo "Value of AGE Inside function: $AGE"
+}
+AGE=40 # Global variable
+setAge
+echo "Value of AGE outside function :"$AGE
+
+setPrice() {
+    local PRICE=50
+    echo "PRICE Value inside local function :" $PRICE
+}
+setPrice
+
+PRICE=60
+echo "PRICE value outside the function": $PRICE
+
+
+echo $PWD # ENV 
+echo $UID # ENV
