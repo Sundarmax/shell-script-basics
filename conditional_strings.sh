9d13fa392d7bbfabd8175881bdfdc8187d2b65fe
sundar <sundar.info22@gmail.com>
2025-01-11 12:37:31 +0530

    conditonal update

--- conditional_strings.sh
+++ conditional_strings.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+: '
+SHELL SCRIPT
+CONDITIONAL STATEMENTS
+'
+file="todo.gz"
 if [[ "one" == "one" ]]; then
     echo "Strings are equal."
 fi
@@ -6,3 +11,22 @@ fi
 if [[ "one" != "two" ]]; then
     echo "strings are not equal"
 fi
+
+if [[ "aaa" < "bbb" ]]; then
+    echo "aaa is smaller"
+fi
+
+if [[ "$file" = *.gz ]]; then
+    echo "Found gzip file : $file"
+fi
+
+list="foo bar baz"
+
+for word in $list; do
+    echo "$word"
+done
+
+array=($list)
+echo "Zeroth: ${array[0]}"  
+echo "First: ${array[1]}"  
+echo "Whole Array: ${array[*]}" 
