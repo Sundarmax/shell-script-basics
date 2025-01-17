138d10d86adb694bf58b1c832602fbce13a20e4a
sundar <sundar.info22@gmail.com>
2025-01-17 16:50:11 +0530

    if elseif else update

--- if_else.sh
+++ if_else.sh
@@ -6,3 +6,13 @@ if [[ $age -gt 60 ]]; then
 else
     echo "Not Senior citizen"
 fi
+
+number=21
+
+if [ $number -gt 30 ]; then
+    echo "Given number is greater than 30"
+elif [ $number -lt 30 ]; then
+    echo "Given number is lesser than 30"
+else
+    echo "Invalid input"
+fi
