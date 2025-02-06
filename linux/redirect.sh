c34566dc2b6baf2d802c5e280368688029b310ba
sundarmax <sundar.info22@gmail.com>
2025-02-05 22:53:32 -0500

    redirect

--- linux/redirect.sh
+++ linux/redirect.sh
@@ -5,7 +5,8 @@ set -x # debug
 # output redirection
 # write a command out to file
 
-ls > test.txt
+ls > test.txt # truncate the file and store the result.
 
+ls -l >> test.txt # append the result to existing file.
 # input redirection.
 wc -l < test.txt
