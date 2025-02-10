37e2097febaa17a1cd38721961cad5e259805ca3
sundarmax <sundar.info22@gmail.com>
2025-02-10 00:55:17 -0500

    sh operators

--- operator.sh
+++ operator.sh
@@ -1,4 +1,45 @@
 #!/bin/bash
 
-rs=$((2 + 4))
-echo "Add: $rs"
+num1=10
+num2=20
+
+# Arithmetic 
+add=$((num1 + num2))
+diff=$((num1 - num2))
+prod=$((num1 * num2))
+quot=$((num1 / num2))
+mod=$((num1 % num2))
+
+echo "$add $diff $prod $quot $mod"
+
+# Relational
+if [ $num1 -lt $num2 ]; then
+    echo "$num1 is less than num2"
+fi
+
+if [ $num1 -eq 10 ]; then
+    echo "$num1 is equal to 10"
+fi
+
+# Logical
+x=10
+y=5
+
+if [ $x -gt $y ] && [ $x -eq 10 ]; then
+    echo "Both conditions are true."
+fi
+
+
+# String
+str1="hello"
+world="world"
+
+if [ "$str1" = "$str2" ]; then
+    echo "Strings are equal"
+else
+    echo "Strings are not equal"
+fi
+
+if [ -n "$str1" ]; then
+    echo "string is not empty"
+fi
